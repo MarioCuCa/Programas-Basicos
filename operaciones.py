0d2125d4974bd2c3d03b49d1ab4346dbefbb1723
MarioCuCa <52554590+MarioCuCa@users.noreply.github.com>
2019-07-04 22:12:31 -0500

    suma, resta, multiplicación, división
    Te pide 2 números y luego realiza las 4 operaciones matemáticas básicas.

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+print("Vamos a realizar 4 operaciones basicas con 2 numeros: ")
+numero1 = int(input("Primer numero: "))
+numero2 = int(input("Segundo numero:"))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("La suma es:",suma)
+print("La resta es:",resta)
+print("La multiplicacion es:",multiplicacion)
+print("La division es:",division)
