a53b75cb095e474603a3a3d23f3cf5c27f307085
MarioCuCa <52554590+MarioCuCa@users.noreply.github.com>
2019-07-04 22:07:59 -0500

    Edad
    Te pide tu nombre, luego tu edad. Te lo lista ambos.

---
+++ edad.py
@@ -0,0 +1,10 @@
+name = input("Hola, como te llamas? ")
+print("Un gusto " + name.capitalize())
+
+edad = input("Que edad tienes? ")
+print("Genial, ya tienes "+edad)
+
+nueva_edad = int(edad) + 2
+
+print("Que rapido se pasa el tiempo")
+print("Entonces tu edad en el 2021 sera de",nueva_edad)
