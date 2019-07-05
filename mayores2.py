a6f334202a571900447746d56a8c8495cc2fa0e3
MarioCuCa <52554590+MarioCuCa@users.noreply.github.com>
2019-07-04 22:11:10 -0500

    Orden de mayor a menor - 2 números
    Te pide 2 números y luego te los ordena de mayor a menor

---
+++ mayores2.py
@@ -0,0 +1,10 @@
+print("Vamos a comparar dos numero, quien es el mayor")
+numero1 = int(input("Numero 1: "))
+numero2 = int(input("Numero 2: "))
+
+if numero1 > numero2:
+	print("El numero 1 es mayor que el numero 2")
+	print(numero1,'>',numero2)
+else:
+	print("El numero 2 es mayor que el numero 1")
+	print(numero2,">",numero1)
